bae26d4cf0edb3fba96c646b1d6765f4c1e79e8a
Younes Jaaidi <younes@marmicode.io>
2024-11-20 22:54:08 +0100

    feat: ✨ add landing

--- apps/cookbook/src/pages/index.tsx
+++ apps/cookbook/src/pages/index.tsx
@@ -1,4 +1,34 @@
-import React from 'react';
-import { Redirect } from '@docusaurus/router';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import useBaseUrl from '@docusaurus/useBaseUrl';
+import Layout from '@theme/Layout';
 
-export default () => <Redirect to="/nx/intro" />;
+import styles from './index.module.css';
+
+export default function Home(): JSX.Element {
+  const { siteConfig } = useDocusaurusContext();
+  return (
+    <Layout
+      title={`Hello from ${siteConfig.title}`}
+      description="Description will go into a meta tag in <head />"
+    >
+      <HomepageHeader />
+    </Layout>
+  );
+}
+
+function HomepageHeader() {
+  return (
+    <header className={styles.heroBanner}>
+      <div className="container">
+        <img
+          alt="Marmicode Pot"
+          className={styles.heroLogo}
+          src={useBaseUrl('/img/hero.png')}
+          height="300"
+        />
+        <p className={styles.heroSubtitle}>MARMICODE</p>
+        <p className={styles.heroSubtitleSecondary}>COOKBOOK</p>
+      </div>
+    </header>
+  );
+}
