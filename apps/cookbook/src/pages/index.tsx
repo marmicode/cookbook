4b1d6560b4240a19ad83b1394e52131005e4e6b9
Younes Jaaidi <younes@marmicode.io>
2024-11-21 00:27:31 +0100

    feat: ✨ improve landing layout

--- apps/cookbook/src/pages/index.tsx
+++ apps/cookbook/src/pages/index.tsx
@@ -12,6 +12,7 @@ export default function Home(): JSX.Element {
   const { siteConfig } = useDocusaurusContext();
   return (
     <Layout
+      wrapperClassName={styles.wrapper}
       title={siteConfig.tagline}
       description={
         'Ingredients & Recipes for Cooking Delicious Apps with Angular & Nx'
@@ -23,32 +24,38 @@ export default function Home(): JSX.Element {
 }
 
 function HomepageHeader() {
-  const { siteConfig } = useDocusaurusContext();
   return (
     <header className={styles.heroBanner}>
-      <div className="container">
-        <img
-          alt="Marmicode Pot"
-          className={styles.heroLogo}
-          src={useBaseUrl('/img/hero.png')}
-          height="300"
-        />
-        <p className={clsx(styles.heroSubtitle)}>MARMICODE</p>
-        <p className={clsx(styles.heroSubtitle, styles.heroSubtitleSecondary)}>
-          COOKBOOK
-        </p>
-        <p className={styles.heroTagline}>{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
-          <Links />
-        </div>
+      <div className={clsx('container', styles.container)}>
+        <Hero />
+        <Buttons />
       </div>
     </header>
   );
 }
 
-function Links() {
+function Hero() {
+  const { siteConfig } = useDocusaurusContext();
   return (
-    <>
+    <section>
+      <img
+        alt="Marmicode Pot"
+        className={styles.heroLogo}
+        src={useBaseUrl('/img/hero.png')}
+        height="300"
+      />
+      <p className={clsx(styles.heroSubtitle)}>MARMICODE</p>
+      <p className={clsx(styles.heroSubtitle, styles.heroSubtitleSecondary)}>
+        COOKBOOK
+      </p>
+      <p className={styles.heroTagline}>{siteConfig.tagline}</p>
+    </section>
+  );
+}
+
+function Buttons() {
+  return (
+    <div className={styles.buttons}>
       {[
         {
           href: '/angular/why-vitest',
@@ -70,13 +77,13 @@ function Links() {
         },
       ].map(({ href, content }) => (
         <Link
-          className={clsx('button button--secondary button--lg', styles.link)}
+          className={clsx('button button--secondary button--lg', styles.button)}
           key={href}
           to={href}
         >
           {content}
         </Link>
       ))}
-    </>
+    </div>
   );
 }
