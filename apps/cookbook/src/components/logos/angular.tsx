72a267645976faf1d3ff774e0d9e9be010a6b919
Younes Jaaidi <younes@marmicode.io>
2024-11-21 01:27:36 +0100

    fix: 🐞 fix logo warning

--- apps/cookbook/src/components/logos/angular.tsx
+++ apps/cookbook/src/components/logos/angular.tsx
@@ -6,7 +6,7 @@ export function Angular() {
       viewBox="0 0 223 236"
       width="32"
     >
-      <g clip-path="url(#a)">
+      <g clipPath="url(#a)">
         <path
           fill="url(#b)"
           d="m222.077 39.192-8.019 125.923L137.387 0l84.69 39.192Zm-53.105 162.825-57.933 33.056-57.934-33.056 11.783-28.556h92.301l11.783 28.556ZM111.039 62.675l30.357 73.803H80.681l30.358-73.803ZM7.937 165.115 0 39.192 84.69 0 7.937 165.115Z"
@@ -25,12 +25,12 @@ export function Angular() {
           y2="129.722"
           gradientUnits="userSpaceOnUse"
         >
-          <stop stop-color="#E40035"></stop>
-          <stop offset=".24" stop-color="#F60A48"></stop>
-          <stop offset=".352" stop-color="#F20755"></stop>
-          <stop offset=".494" stop-color="#DC087D"></stop>
-          <stop offset=".745" stop-color="#9717E7"></stop>
-          <stop offset="1" stop-color="#6C00F5"></stop>
+          <stop stopColor="#E40035"></stop>
+          <stop offset=".24" stopColor="#F60A48"></stop>
+          <stop offset=".352" stopColor="#F20755"></stop>
+          <stop offset=".494" stopColor="#DC087D"></stop>
+          <stop offset=".745" stopColor="#9717E7"></stop>
+          <stop offset="1" stopColor="#6C00F5"></stop>
         </linearGradient>
         <linearGradient
           id="c"
@@ -40,8 +40,8 @@ export function Angular() {
           y2="160.344"
           gradientUnits="userSpaceOnUse"
         >
-          <stop stop-color="#FF31D9"></stop>
-          <stop offset="1" stop-color="#FF5BE1" stop-opacity="0"></stop>
+          <stop stopColor="#FF31D9"></stop>
+          <stop offset="1" stopColor="#FF5BE1" stopOpacity="0"></stop>
         </linearGradient>
         <clipPath id="a">
           <path fill="#fff" d="M0 0h223v236H0z"></path>
