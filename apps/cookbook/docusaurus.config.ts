6738a72187cf681f1e4f9fb9f6e4a40392abf01c
Younes Jaaidi <younes@marmicode.io>
2025-05-19 15:05:35 +0200

    feat: ✨ add last update time

--- apps/cookbook/docusaurus.config.ts
+++ apps/cookbook/docusaurus.config.ts
@@ -45,6 +45,7 @@ const config: Config = {
           editUrl:
             'https://github.com/marmicode/cookbook/tree/main/apps/cookbook',
           routeBasePath: '/',
+          showLastUpdateTime: true,
         },
         blog: false,
         theme: {
